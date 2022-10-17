1d1e62fc82d344575e75cb7a313ff311bd23b68a
Administrator <Administrator@WIN-171A8TGB90A>
2022-10-18 00:33:47 +0700

    Step1

---
+++ src/main/java/pabd/migratedb/MigratedbApplication.java
@@ -0,0 +1,13 @@
+package pabd.migratedb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MigratedbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MigratedbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/pabd/migratedb/MigratedbApplicationTests.java
@@ -0,0 +1,13 @@
+package pabd.migratedb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MigratedbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
